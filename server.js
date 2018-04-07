a6a20d2953573ee140cc3cb83921e342b944bccc
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-04-07 10:10:52 -0700

    First commit

---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const bCrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const database = {
+	users: [
+		{
+			id: '123',
+			name: 'John',
+			email: 'john@gmail.com',
+			password: 'beans',
+			entries: 0,
+			joined: new Date()
+		}
+	]
+}
+
+app.get('/', (req, res) => {
+	res.send(database.users);
+});
+
+app.post('/signin', (req, res) => {
+	if (req.body.email === database.users[0].email 
+			&& req.body.password === database.users[0].password) {
+		res.json(database.users[0]);
+	} else {
+		res.status(400).json('Error');
+	}
+});
+
+app.post('/register', (req, res) => {
+	const { email,name,password } = req.body;
+	database.users.push({
+		id: '124',
+		name,
+		email,
+		password,
+		entries: 0,
+		joined: new Date()
+	});
+	res.json(database.users[database.users.length -1]);
+});
+
+app.get('/profile/:id', (req, res) => {
+	const { id } = req.params;
+	let found = false;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			return res.json(user);
+		} 
+		if (!found) {
+			res.status(400).json('not found');
+		}
+	});
+});
+
+app.put('/image', (req, res) => {
+	const { id } = req.body;
+	let found = false;
+	database.users.forEach(user => {
+		if (user.id === id) {
+			found = true;
+			user.entries++;
+			return res.json(user.entries);
+		} 
+	});
+		if (!found) {
+			res.status(400).json('not found');
+		}
+});
+
+app.listen(3000, ()=> {
+	console.log('running smooth!');
+});
+
+/*
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userIf --> GET = user
+/image --> PUT --> user
+
+*/
