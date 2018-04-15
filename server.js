3d8ce1449520b562018ba2bfb0f0c84d68a1f36c
Ozymandias9000 <[nick_murphy@sbcglobal.net]>
2018-04-15 10:59:06 -0700

    deploy corsbug10

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ const db = require('knex')({
 app.use(bodyParser.json());
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    req.header("Access-Control-Allow-Origin", "*");
+    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
